1e5700708dcf20e213a20692c61ca0bd5142a351
PuperUser <cpp-developer@mail.ru>
2022-12-01 12:27:25 +0300

    operator int()

---
+++ operator_int.cpp
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+// Рекурсивный вызов operator int(), происходит stack overflow
+
+class Foo {
+public:
+    operator int() const {
+        printf(".");
+        return *this;
+    }
+};
+
+int main() {
+    Foo foo;
+    int i = foo;
+    return 0;
+}
